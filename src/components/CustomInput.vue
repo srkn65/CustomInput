44dd13066770ec2c18f095b551cef979b1976512
srkn65 <korkacserkan@gmail.com>
2021-12-16 21:33:11 +0300

    $listeners && v-model

---
+++ src/components/CustomInput.vue
@@ -0,0 +1,16 @@
+
+// CustomInput.vue
+<template>
+    <!-- $listeners -->
+    <!-- <input v-on="$listeners" /> -->
+    <!-- v-model -->
+    <input @input="$emit('input', $event.target.value)" />
+</template>
+<script>
+export default {
+    name: 'CustomInput'
+}
+</script>
+
+
+
